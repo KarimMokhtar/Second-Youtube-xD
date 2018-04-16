cd321102ab094aa0af7054cb27a4638b8976336c
karim mokhtar <kmokhtar@abuerdan.com>
2018-04-16 07:36:57 +0200

    video list and video ditail are in progress with search component too

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
-
-
+import VideoList from './components/video_list';
+import VideoDtail from './components/video_detail';
 const API_KEY = 'AIzaSyDl_ATMZ4n_l94OJD_PWgyzT40O_sVVGbw';
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
 
-        </div>
-    );
+
+class App extends Component {
+    constructor(props){
+        super(props);
+        
+        this.state = { videos: [] };
+        YTSearch({key:API_KEY,term:'surfboards'}, (result)=> {
+            this.setState({videos:result});
+        });
+    }
+    render(){
+        return (
+            <div>
+                <SearchBar />
+                <VideoDtail video = {this.state.videos[0]}/>
+                <VideoList videos = {this.state.videos}/>
+            </div>
+        );
+    }
 }
 ReactDOM.render(<App />, document.querySelector(".container"));
